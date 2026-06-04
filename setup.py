73ebef2f471d25e6dd14dc7e2f043c21618313f8
Aslamvs1 <aslua369@gmail.com>
2026-06-04 11:21:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Aslam",
+    author_email="aslamvs9371@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
